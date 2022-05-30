1218ddb8b87b44e1981914bdbe2cd2e54385a8d0
Gero Wallisch <gero.wallisch@me.com>
2022-05-30 16:15:01 +0200

    Update HostingTileComponent.php

--- src/HostingTileComponent.php
+++ src/HostingTileComponent.php
@@ -21,8 +21,11 @@ class HostingTileComponent extends Component
         $hostingStore = HostingStore::make();
 
         return view('dashboard-hosting-tile::tile', [
-            'website' => config('dashboard.tiles.hosting.url'),
-            'server' => $hostingStore->getData(),
+            'website'         => config('dashboard.tiles.hosting.url'),
+            'server'          => $hostingStore->getData(),
+            'anbieter'        => $hostingStore->getData()['org'],
+            'ip'              => $hostingStore->getData()['query'],
+            'serverName'      => $hostingStore->getData()['reverse'],
             'lastUpdateTime'  => date('H:i:s', strtotime($hostingStore->getLastUpdateTime())),
             'lastUpdateDate'  => date('d.m.Y', strtotime($hostingStore->getLastUpdateDate())),
         ]);
